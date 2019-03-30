a7ee7bf07913f909ed2345b212f15e4aeccb0c81
Edgar Zavala <ezavile@gmail.com>
2019-03-30 02:33:56 -0600

    window ready - script blocking

---
+++ src/js/13-ready.js
@@ -0,0 +1,9 @@
+function domReady() {
+  console.log('--- Body Inner HTML ---');
+  console.log(document.body.innerHTML);
+}
+
+function getBodyText() {
+  console.log('--- Body Inner Text ---');
+  console.log(document.body.innerText);
+}
