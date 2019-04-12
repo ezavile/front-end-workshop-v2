fd66324f2ef5103dcbbf76ab9aa693c20fbdbd6b
Edgar Zavala <ezavile@gmail.com>
2019-04-12 01:45:54 -0500

    feat: integrate parcel - gitignore - npm config - nvmrc - package-lock - node_modules - dependencies - devDependencies - semver - parcel bundler

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require('reset-css');
+require('./css/globals.css');
+require('./css/icon-fonts.css');
+require('./css/utils.css');
+require('./css/link.css');
+require('./css/header.css');
+require('./css/cover.css');
+require('./css/title.css');
+require('./css/button.css');
+require('./css/avatar.css');
+require('./css/list-campaigns.css');
+require('./css/campaign-card.css');
+require('./css/footer.css');
